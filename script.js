5a538339bc09448bc9ff97b4a65c45bee156690c
anikahamedofc <anikahamedofc123@gmail.com>
2022-12-13 18:27:52 +0600

    class_two

---
+++ script.js
@@ -0,0 +1,79 @@
+/* There are Two DataType */
+
+//1.OBJECT
+//2.PRIMITIVE
+
+/* Primitive  have  7 DataType  */
+
+
+/*
+1.STRING
+2.NUMBER
+3.BOOLEAN
+==========
+4.UNDEFINED
+5.NULL
+6.SYMBLE
+7.BIGINT
+*/
+
+//String
+let fiestName = "Anik";
+console.log(fiestName);
+
+//Number
+let age = 28;
+console.log(age);
+
+//Boolean
+let isProgrammer = true;
+console.log(isProgrammer);
+
+//UnDefined
+let city;
+console.log(city);
+
+//Null
+let haveCar = null;
+console.log(haveCar);
+
+/* typeof is an Operator */
+
+console.log(typeof fiestName);
+console.log(typeof age);
+console.log(typeof isProgrammer);
+console.log(typeof city);
+console.log(typeof haveCar); // javaScript Bug
+
+/* JavaScript Arithmetic */
+
+let x = 10;
+let y = 30;
+
+let z = x + y;
+
+console.log(z);
+
+let a = 40-20;
+console.log(a);
+
+let b = 30*2;
+console.log(b);
+
+
+let c = 30/2;
+console.log(c);
+
+let v = 19%2;
+console.log(v);
+
+let w = 12;
+
+w +=8;
+console.log(w);
+
+
+let e = 3**3;
+console.log(e);
+
+
